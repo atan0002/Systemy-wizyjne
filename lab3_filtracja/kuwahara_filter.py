247a92278a0b9e399b479ab3b4334fbce5e2c06c
atan0002 <natalia.wisniewsqa@gmail.com>
2023-03-12 17:30:14 +0100

    change of image

--- lab3_filtracja/kuwahara_filter.py
+++ lab3_filtracja/kuwahara_filter.py
@@ -4,12 +4,30 @@ from matplotlib import pyplot as plt
 
 
 
-img=cv2.imread('./Images/whitepixels.png',cv2.IMREAD_GRAYSCALE)
+img=cv2.imread('./Images/lenna.png',cv2.IMREAD_GRAYSCALE)
+
+def img_white_pix(img_):
+    
+    img_white=img_.copy()
+    rows=img_white.shape[0]
+    columns=img_white.shape[1]
+    
+    for i in range(0,rows,3):
+        third_j=0
+        for j in range(columns):
+            
+            if ((j+1)-third_j)==3:
+                img_white[i,j]=255
+
+                third_j=j+1
+
+    return img_white
 
 
-def kuwahara_filter(L,imgl):
+def kuwahara_filter(w,imgl):
 
-    width=2*L+1
+    width=w
+    L=int((width-1)/2)
     region_width=width-L
     rows=imgl.shape[0]
     columns=imgl.shape[1]
@@ -18,31 +36,40 @@ def kuwahara_filter(L,imgl):
 
 
 
-    for i in range(1,rows-L-1):
+    for i in range(1,rows-width):
 
-        for j in range(1,columns-L-1):
+        for j in range(1,columns-width):
             
             window=imgl[i:i+width,j:j+width]
             rows_w=window.shape[0]
             columns_w=window.shape[1]
             region1=window[0:region_width,0:region_width]
+            regs=region1.shape
+
             reg1mean,reg1stddev=cv2.meanStdDev(region1)
        
-            stat['mean'].append(reg1mean)
-            stat['deviat'].append(reg1stddev)
-           
+            stat['mean'].append(reg1mean[0][0])
+            stat['deviat'].append(reg1stddev[0][0])
             region2=window[0:region_width,region_width-1:columns_w]
             reg2mean,reg2stddev=cv2.meanStdDev(region2)
-            stat['mean'].append(reg2mean)
-            stat['deviat'].append(reg2stddev)
+
+            regs=region2.shape
+            
+            stat['mean'].append(reg2mean[0][0])
+            stat['deviat'].append(reg2stddev[0][0])
             region3=window[region_width-1:rows_w,region_width-1:columns_w]
             reg3mean,reg3stddev=cv2.meanStdDev(region3)
-            stat['mean'].append(reg3mean)
-            stat['deviat'].append(reg3stddev)
+
+            regs=region3.shape
+
+            stat['mean'].append(reg3mean[0][0])
+            stat['deviat'].append(reg3stddev[0][0])
             region4=window[region_width-1:rows_w,0:region_width]
             reg4mean,reg4stddev=cv2.meanStdDev(region4)
-            stat['mean'].append(reg4mean)
-            stat['deviat'].append(reg4stddev)
+            stat['mean'].append(reg4mean[0][0])
+            stat['deviat'].append(reg4stddev[0][0])
+
+            regs=region4.shape
 
 
             min_dev_ind=stat['deviat'].index(min(stat['deviat']))
@@ -53,7 +80,10 @@ def kuwahara_filter(L,imgl):
 
     return img2 
 
-img_kuh=kuwahara_filter(2,img)
+
+imgW=img_white_pix(img)
+
+img_kuh=kuwahara_filter(5,imgW)
 
 while True:
     
